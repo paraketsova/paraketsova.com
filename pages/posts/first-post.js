225186812f6458fc374e163e724f3d701b6ebf9f
Mariia Paraketsova <paraketsova@gmail.com>
2023-02-24 15:58:53 +0100

    Adding Global CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,6 @@
-import Link from 'next/link';
 import Head from 'next/head';
-import Script from 'next/script';
+import Link from 'next/link';
 import Layout from '../../components/layout';
-
 
 export default function FirstPost() {
   return (
@@ -10,16 +8,9 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
-      />
       <h1>First Post</h1>
       <h2>
-        <Link href="/">Back to home</Link>
+        <Link href="/">← Back to home</Link>
       </h2>
     </Layout>
   );
