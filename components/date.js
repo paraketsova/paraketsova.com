a76acef23958d1975db0e59e45523e7577765658
Mariia Paraketsova <paraketsova@gmail.com>
2023-09-24 13:49:09 +0200

    Remove leftover old files

--- components/date.js
+++
@@ -1,7 +0,0 @@
-import { parseISO, format } from 'date-fns';
-
-export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
-}
-// different format() string options on the date-fns website - https://date-fns.org/v2.16.1/docs/format
